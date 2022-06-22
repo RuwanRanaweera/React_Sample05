c2c23ddd1355e9aa7249c590ee233d7936c466c4
Ruwan Ranaweera <rwnranaweera@gmail.com>
2022-06-22 09:49:13 +0530

    routing practice

---
+++ src/Shop.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function Shop() {
+  //const [loading, setLoading] = useState(false);
+  useEffect(()=> {
+      fetchItems();
+  },[]);
+
+  const [items, setItems] = useState([]);
+
+
+
+  const fetchItems = async ()=> {
+    const data = await fetch('https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get');
+    
+
+    const items = await data.json();
+    console.log(items.items);
+    setItems(items.items);
+
+  };
+  return (
+    <div>
+        {items.map(item =>(
+          <h1 key={item.itemid}>{item.name}</h1>
+        ))}
+
+    </div>
+  );
+}
+
+export default Shop;
